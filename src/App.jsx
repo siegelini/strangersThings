231d86f6b897910ab02ab43b498a310878a3e63a
mheuangsayaseng <maggieheuang@gmail.com>
2023-04-18 17:08:43 -0500

    maggie's commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   console.log("Token from App.jsx", token);
   return (
     <div className="App">
-      <h1>Stranger's Things</h1>
+      <h1>Welcome to Stranger's Things</h1>
       <nav>
         <Link to="/">Login</Link>
         <Link to="/allposts">All Posts</Link>
