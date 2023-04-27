0b55060a291085a711eb4df8b8675e052986ae66
Adam Siegel <siegelini@icloud.com>
2023-04-27 12:37:53 -0400

    added to myProfile, added css for myProfile

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 // import { fetchMessages } from "../api";
+import AuthProvider from "./AuthProvider";
+
 export default function MyProfile() {
   return (
-    <div classname="personal-post">
-      <h1>All Your Posts Here</h1>
+    <div className="profile-page">
+      <div className="personal-post">
+        <h1>All Your Posts Here</h1>
+        <p>Your posts Here</p>
+      </div>
+      <div className="my-messages">
+        <h1>Messages</h1>
+        <p>Your messages here</p>
+      </div>
     </div>
   );
 }
